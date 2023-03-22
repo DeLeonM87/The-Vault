a6a0a0f4f115939508e7017ac31e0489216eb46e
DeLeonM87 <127151049+DeLeonM87@users.noreply.github.com>
2023-03-22 02:16:28 -0500

    Add files via upload
    Built a vault that requires three mathematical calculations to generate the three codes in a combination. Use the JavaScript console or the script block within an HTML page to create three variables. Each variable will be the result of each calculation (three in total). The combination of the lock is 10 - 40 - 39.

---
+++ main.js
@@ -0,0 +1,17 @@
+//Psudocode: Getting a warning notification set for the secure vault.
+console.log ("hello world")
+
+//This operation should have the outcome of 10
+let num1 = 5 + 5;
+
+//The variable num should be reassigned and changed to 40
+let num2 = num1 * 4;
+
+//The variable num should be reassigned and changed to 39
+let num3 = num2 -1;
+
+//const info = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination: $ {num1} - ${num2} - ${num3}!';
+
+const info = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39';
+
+alert (info)
